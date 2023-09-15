fa009adf85f2ac516761786b649c52049db00c40
sjsimpson <spencerjsimpson@gmail.com>
2023-09-15 17:46:15 -0600

    fix: add OtherIcon type, to allow custom icons while maintaining intellisense

--- src/Icon/Icon.types.ts
+++ src/Icon/Icon.types.ts
@@ -1,4 +1,8 @@
+// Doing this for improved intellisense, but wtill allowing users to input any
+// material icon string
+type OtherIcon = string & { materialIcon?: any }
 export type IconStyles =
+  | 'account_circle'
   | 'home'
   | 'menu'
   | 'menu_open'
@@ -15,6 +19,9 @@ export type IconStyles =
   | 'list'
   | 'construction'
   | 'work'
+  | 'arrow_back'
+  | 'arrow_forward'
+  | OtherIcon
 
 export type IconColors =
   | 'primary'
